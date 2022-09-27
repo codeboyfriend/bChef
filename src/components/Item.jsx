f5e1b988c56fe9d015b063608b269e685efc4363
codeboyfriend <mkinggrafix@gmail.com>
2022-09-27 12:42:29 -0700

    Added

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,24 +2,23 @@ import { useContext } from "react";
 import GlobalContext from "../context/GlobalContext";
 
 const Item = ({
-  items
+  items,
+  SwiperSlide
 }) => {
   const {
     fetchEachData,
-    fetchData,
     setUserInput
-  } = useContext(GlobalContext);  
+  } = useContext(GlobalContext);
 
-  // console.log(userInput)
   return (
-    <div className="box max-w-[300px] bg-bgColor text-white" onClick={() => {
+    <div className="boxes max-w-[280px] bg-bgColor text-white" onClick={() => {
       setUserInput(items?.name)
       fetchEachData()
     }}>
       <div>
         <img className="w-[100%]" src={items?.cover} alt="" />
       </div>
-      <div className="recipe text-xl p-2">{items?.name}</div>
+      <div className="recipe text-sm md:text-xl p-2">{items?.name}</div>
     </div>
   )
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,34 @@
 import { useContext } from "react";
 import GlobalContext from "../context/GlobalContext";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
 import Item from "./Item";
 
 const Map = () => {
     const { 
         allData 
     } = useContext(GlobalContext);
-
+    
   return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={2}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
     <div className="px-6 md:px-10 py-4">
         <div className="mb-2 text-xl font-bold">Trending:</div>
-        <div className="flex flex-col md:flex-row space-x-3">
+        <div className="box">
             {allData.map((items) => <Item 
                 key={items.id}
-                items={items} 
+                items={items}
+                SwiperSlide={SwiperSlide} 
             />)}
         </div>
     </div>
+    </Swiper>
   )
 }
 
