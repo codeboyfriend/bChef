53312be54d7e108b94aa148fe15daad3f0cbca96
codeboyfriend <mkinggrafix@gmail.com>
2022-10-16 13:38:05 -0700

    Modified Offline Page

--- src/Offline.js
+++ src/Offline.js
@@ -1,11 +1,13 @@
 import "./css/style.css";
 
-function App() {
+function Offline() {
   return (
     <div className="Offline flex felx-col items-center justify-center min-h-[100vh] text-black">
       Connect to internet connect to search for recipe
+      
+      <div className="text-black">Please go online to check recipe</div>
     </div>
   );
 }
 
-export default App;
+export default Offline;
